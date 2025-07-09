bb7675ca81c172532e5a2b79fb99c2ac0cb69472
Mathew <mattcohen526@gmail.com>
2025-07-08 21:43:51 -0400

    fix cross compile issue by wrapping unix-only directive with pre-processor directives

--- main.cpp
+++ main.cpp
@@ -10,7 +10,9 @@ int main(int argc, char *argv[])
 {
     // This line of code will force your system compositor to use X11 or a compatibility layer like Xwayland
     // This obviously does not apply to Win/Mac
-    qputenv("QT_QPA_PLATFORM","xcb");
+    #ifdef Q_OS_UNIX
+        qputenv("QT_QPA_PLATFORM", "xcb");
+    #endif
 
     QApplication a(argc, argv);
     MainWindow w;
